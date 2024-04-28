5757a00352bc62e6bab33006de0053c1b8a21905
nabrego <93846936+nabrego@users.noreply.github.com>
2024-04-27 19:49:36 -0700

    Added Log in page

--- dcmh-inventory/src/app/login-page/page.tsx
+++ dcmh-inventory/src/app/login-page/page.tsx
@@ -1,7 +1,33 @@
 export default function Home() {
     return (
+        /* The Log-In text */
         <div className = "text-black">
-            <h1 className = "text-6x1 font-medium text-center mt-50">Log-In</h1>
+            <p className = "text-4xl font-medium text-center mt-20">Log-In</p>
+            
+            
+            <div className = "flex justify-center mt-20"> 
+                <div className = "border border-black border-4 p-4 w-full sm:w-80 bg-white">
+                    <div className = "text-black">
+                        <p className = "text-xl font-medium text-left">Username</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "flex justify-center mt-5">
+                <div className = "border border-black border-4 p-4 w-full sm:w-80 bg-white">
+                    <div className = "text-black">
+                        <p className = "text-xl font-medium text-left">Password</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "flex justify-center mt-10">
+                <div className = "p-4 w-full sm:w-80 bg-gray-400">
+                    <div className = "text-black">
+                        <p className = "text-3xl font-medium text-center">Sign-In</p>
+                    </div>
+                </div>
+            </div>
 
 
         </div>
