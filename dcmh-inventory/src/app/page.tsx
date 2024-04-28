9d63fdedfb0d2e3fab0ea0edea86aad9f7431ddf
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-04-28 13:29:51 -0700

    fixed some merge issues

--- dcmh-inventory/src/app/page.tsx
+++ dcmh-inventory/src/app/page.tsx
@@ -34,5 +34,33 @@ export default function Home({params}: any) {
     <div className="text-black">
       <Navbar/>
 
-  );
-}
+
+      <h1 className="text-6xl font-medium text-center mt-40 ">Hi we need:</h1>
+      <h2>{message}</h2>
+      {items && items.map((item: InventoryItem) => (
+      <div key={item._id} className="mt-20 mx-28 bg-gray-300">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <span className="text-lg font-bold">{item.goalAmount}</span>
+          </div>
+
+          <div className="flex items-center ml-4">
+            <span className="text-lg">{item.name}</span>
+          </div>
+
+          <div className="flex items-center ml-4">
+            <div className="h-8 w-8 bg-green-500 rounded-full"></div>
+          </div>
+
+          <div className="flex items-center ml-4">
+            <span className="text-lg font-semibold text-red-600">
+              High Priority
+            </span>
+          </div>
+        </div>
+      </div>
+
+
+    ))}
+
+    </div>)}
