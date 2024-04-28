e85fb7be0446d96538363f1176a753d92afe58a0
nabrego <93846936+nabrego@users.noreply.github.com>
2024-04-27 19:00:14 -0700

    Created the login page
    Created the login page

---
+++ dcmh-inventory/src/app/login-page/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className = "text-black">
+            <h1 className = "text-6x1 font-medium text-center mt-50">Log-In</h1>
+
+
+        </div>
+    );
+}
