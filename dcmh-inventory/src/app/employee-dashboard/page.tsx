5e79958c0bb72486f6209deae8d3f735dc00e83e
nabrego <93846936+nabrego@users.noreply.github.com>
2024-04-28 08:06:57 -0700

    Updated login and employee dashboard

--- dcmh-inventory/src/app/employee-dashboard/page.tsx
+++ dcmh-inventory/src/app/employee-dashboard/page.tsx
@@ -2,8 +2,58 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-black">
-      
+    <main>
+    <div className = "flex">
+      <button className = "bg-zinc-300 p-10 ml-96 mt-20">
+        <div className = "text-black">
+          <p className = "text-lg font-medium text-left">Key Data Statistic</p>
+        </div>
+      </button>
+
+      <button className = "bg-zinc-300 p-10 ml-10 mt-20">
+        <div className = "text-black">
+          <p className="text-lg font-medium text-left">Key Data Point</p>
+        </div>
+      </button>
+
+      <button className="bg-zinc-300 p-10 ml-10 mt-20">
+        <div className="text-black">
+          <p className="text-lg font-medium text-left">Key Data Point</p>
+        </div>
+      </button>
     </div>
+
+    <div className = "flex justify-center">
+      <div className = "bg-zinc-300 p-40 mt-10 w-full w-1/2 relative">
+          <div className = "absolute top-5 left-5 bg-white w-4/5 h-10">
+            <div className = "text-black">
+              <p className = "font-medium text-2xl mt-1 ml-3">Search</p>
+            </div>
+          </div>
+          <div className = "absolute bg-white right-5 top-5 w-24 h-10">
+            <div className = "text-black">
+              <p className = "font-medium text-2xl mt-1 text-center">Add</p>
+            </div>
+          </div>
+          <div className = "absolute top-20 left-5">
+            <div className = "text-black flex">
+              <p className=" font-medium text-xl">Priority</p>
+              <div className="ml-1 w-4 h-4 bg-red-500 rounded-full"></div>
+              <div className = "ml-5">
+                <p className = "font-medium text-xl">Item Name</p>
+              </div>
+              <div className = "ml-14">
+                <p className = "font-medium text-xl">Count</p>
+              </div>
+              <div className = "ml-5">
+                <p className = "font-medium text-xl">Tag</p>
+              </div>
+            </div>
+          </div>
+          <div className = "absolute top-32">
+          </div>
+      </div>
+    </div>
+    </main>
   );
 }
--- dcmh-inventory/src/app/login-page/page.tsx
+++ dcmh-inventory/src/app/login-page/page.tsx
@@ -1,12 +1,11 @@
 export default function Home() {
     return (
-        /* The Log-In text */
         <div className = "text-black">
-            <p className = "text-4xl font-medium text-center mt-20">Log-In</p>
+            <p className = "text-4xl font-medium flex justify-center mt-20">Log-In</p>
             
             
             <div className = "flex justify-center mt-20"> 
-                <div className = "border border-black border-4 p-4 w-full sm:w-80 bg-white">
+                <div className = "border border-black border-4 p-4 w-full w-1/4 bg-white">
                     <div className = "text-black">
                         <p className = "text-xl font-medium text-left">Username</p>
                     </div>
@@ -14,7 +13,7 @@ export default function Home() {
             </div>
 
             <div className = "flex justify-center mt-5">
-                <div className = "border border-black border-4 p-4 w-full sm:w-80 bg-white">
+                <div className = "border border-black border-4 p-4 w-full w-1/4 bg-white">
                     <div className = "text-black">
                         <p className = "text-xl font-medium text-left">Password</p>
                     </div>
@@ -22,7 +21,7 @@ export default function Home() {
             </div>
 
             <div className = "flex justify-center mt-10">
-                <div className = "p-4 w-full sm:w-80 bg-gray-400">
+                <div className = "p-4 w-full w-1/4 bg-zinc-400">
                     <div className = "text-black">
                         <p className = "text-3xl font-medium text-center">Sign-In</p>
                     </div>
