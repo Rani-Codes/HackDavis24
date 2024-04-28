96d47faacc202b11bd6d1d11e67c16a794a88d1d
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-04-28 01:52:24 -0700

    Update and Delete api calls created for users

--- dcmh-inventory/backend/index.js
+++ dcmh-inventory/backend/index.js
@@ -98,6 +98,25 @@ app.post('/api/user', async (req, res) => {
   }
 })
 
+//Update user that's in the db, must enter its id
+app.put('/api/user/:id', async (req, res) => {
+
+  try {
+    const { id } = req.params
+    const user = await User.findByIdAndUpdate(id, req.body)
+
+    if(!user) {
+      return res.status(404).json({message: "User not found"})
+    }
+
+    const updatedUser = await User.findById(id)
+    res.status(200).json(updatedUser)
+
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
 //Get all users in the db
 app.get('/api/user', async (req, res) => {
   try {
@@ -120,6 +139,24 @@ app.get('/api/user/:id', async (req, res) => {
     }
 
     res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+//Delete an item that's in the inventory, must enter its id
+app.delete('/api/user/:id', async (req, res) => {
+
+  try {
+    const { id } = req.params
+    const user = await User.findByIdAndDelete(id)
+
+    if(!user) {
+      return res.status(404).json({message: "User not found"})
+    }
+
+    res.status(200).json({message: "User Deleted Successfully"})
+
   } catch (error) {
     res.status(500).json({message: error.message})
   }
