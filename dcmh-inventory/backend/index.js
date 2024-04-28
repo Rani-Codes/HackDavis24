38b579d1817c2f7fb61489ff4a014b71671d7593
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-04-27 22:55:51 -0700

    added mongoDB connection to index.js, created initial schema

--- dcmh-inventory/backend/index.js
+++ dcmh-inventory/backend/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3001, () => {
-    console.log("Server is running on port 30001")
+app.get('/', (req, res) => {
+  res.send('Hello from node API')
 })
+
+
+mongoose.connect('mongodb+srv://Rani:Ku8Z8FsMF5dgCVwv@hackdavis24.xywjix4.mongodb.net/')
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(3001, () => {
+      console.log('Server is running on port 3001')
+    })
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  })
